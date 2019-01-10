ec08a481da9b57c94c7ec98d4d30b6008107a3ee
Марио Мм <mariomm@iMac-Mario.local>
2019-01-10 17:46:04 +0300

    add demo folder for eample

---
+++ build/precache-manifest.071bd3da9295c27f7e36b74799596840.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "97c26aceb5f7fd123c92",
+    "url": "/static/js/main.97c26ace.chunk.js"
+  },
+  {
+    "revision": "da689e89a2e4b0a23e82",
+    "url": "/static/js/1.da689e89.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "62eadc188ef49ef7bec72bfee8552fbc",
+    "url": "/index.html"
+  }
+];
