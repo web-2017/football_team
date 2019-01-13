7e7a04169d13ec3fa89030e47be1d3896fe0197c
Марио Мм <mariomm@iMac-Mario.local>
2019-01-13 23:27:23 +0300

    add olayers card

--- src/components/home/meetPlayers/index.js
+++ src/components/home/meetPlayers/index.js
@@ -1,8 +1,14 @@
 import React, { Component } from "react";
 import SripesBg from "../../../Resources/images/stripes.png";
 import { Tag } from "../../ui/misc";
+import Reveal from "react-reveal";
+
+import HomeCards from "./cards";
 
 export default class MeetPlayers extends Component {
+  state = {
+    show: false,
+  };
   render() {
     const names = [
       { id: 0, text: "Meet", marginBottom: "20px" },
@@ -29,31 +35,43 @@ export default class MeetPlayers extends Component {
     });
 
     return (
-      <div
-        className="home_meetplayers"
-        style={{ background: `#ffffff url(${SripesBg})` }}
+      <Reveal
+        fraction={0.7}
+        onReveal={() => {
+          /* console.log("reveal"); */
+          this.setState({
+            show: true,
+          });
+        }}
       >
-        <div className="container">
-          <div className="home_meetplayers_wrapper">
-            <div className="home_card_wrapper">card</div>
-            <div className="home_text_wrapper">{homeText}</div>
-            <Tag
-              bck="##ffffff"
-              size="27px"
-              color="#0e1731"
-              link={true}
-              linkTo="/the_team"
-              add={{
-                display: "inline-block",
-                marginBottom: "27px",
-                border: "1px solid #0e1731",
-              }}
-            >
-              Meet there here
-            </Tag>
+        <div
+          className="home_meetplayers"
+          style={{ background: `#ffffff url(${SripesBg})` }}
+        >
+          <div className="container">
+            <div className="home_meetplayers_wrapper">
+              <div className="home_card_wrapper">
+                <HomeCards show={this.state.show} />
+              </div>
+              <div className="home_text_wrapper">{homeText}</div>
+              <Tag
+                bck="##ffffff"
+                size="27px"
+                color="#0e1731"
+                link={true}
+                linkTo="/the_team"
+                add={{
+                  display: "inline-block",
+                  marginBottom: "27px",
+                  border: "1px solid #0e1731",
+                }}
+              >
+                Meet there here
+              </Tag>
+            </div>
           </div>
         </div>
-      </div>
+      </Reveal>
     );
   }
 }
